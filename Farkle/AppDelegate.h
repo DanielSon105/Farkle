00fd2bf6ab85f78de6b2b6313cbe1d2b5fa8d64c
DanielSon105 <daniel.g.barrido@gmail.com>
2015-10-08 15:12:10 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Daniel Barrido on 10/8/15.
+//  Copyright © 2015 RunDMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Daniel Barrido on 10/8/15.
+//  Copyright © 2015 RunDMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
